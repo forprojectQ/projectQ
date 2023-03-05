f6487d66cd781120c52522c08674505c4ba8a5b2
mahlukat5 <temmuz.sanaga@hotmail.com>
2023-03-06 02:00:35 +0300

    vehicles_library tablosune 'enabled' eklendi

--- roleplay.sql
+++ roleplay.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : database
-Source Server Version : 80017
+Source Server         : localsw
+Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : roleplay
 
 Target Server Type    : MYSQL
-Target Server Version : 80017
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2023-03-05 23:16:40
+Date: 2023-03-06 01:58:52
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,11 +20,11 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
-  `name` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
-  `password` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
-  `admin` int(11) NOT NULL DEFAULT '0',
-  `serial` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
-  `limit` int(11) NOT NULL DEFAULT '3',
+  `name` text CHARACTER SET latin1 DEFAULT NULL,
+  `password` text CHARACTER SET latin1 DEFAULT NULL,
+  `admin` int(11) NOT NULL DEFAULT 0,
+  `serial` text CHARACTER SET latin1 DEFAULT NULL,
+  `limit` int(11) NOT NULL DEFAULT 3,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -38,19 +38,19 @@ CREATE TABLE `accounts` (
 DROP TABLE IF EXISTS `characters`;
 CREATE TABLE `characters` (
   `id` int(11) NOT NULL,
-  `account` int(11) NOT NULL DEFAULT '0',
-  `name` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
-  `pos` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0,0,5,0,0',
-  `age` int(11) NOT NULL DEFAULT '18',
-  `height` int(11) NOT NULL DEFAULT '175',
-  `weight` int(11) NOT NULL DEFAULT '65',
-  `gender` int(1) NOT NULL DEFAULT '1',
-  `model` int(11) NOT NULL DEFAULT '170',
-  `money` bigint(11) NOT NULL DEFAULT '1000',
-  `hunger` int(11) NOT NULL DEFAULT '100',
-  `thirst` int(11) NOT NULL DEFAULT '100',
-  `dead` int(1) NOT NULL DEFAULT '0',
-  `walk` int(11) NOT NULL DEFAULT '128',
+  `account` int(11) NOT NULL DEFAULT 0,
+  `name` text CHARACTER SET latin1 DEFAULT NULL,
+  `pos` varchar(255) NOT NULL DEFAULT '0,0,5,0,0',
+  `age` int(11) NOT NULL DEFAULT 18,
+  `height` int(11) NOT NULL DEFAULT 175,
+  `weight` int(11) NOT NULL DEFAULT 65,
+  `gender` int(1) NOT NULL DEFAULT 1,
+  `model` int(11) NOT NULL DEFAULT 170,
+  `money` bigint(11) NOT NULL DEFAULT 1000,
+  `hunger` int(11) NOT NULL DEFAULT 100,
+  `thirst` int(11) NOT NULL DEFAULT 100,
+  `dead` int(1) NOT NULL DEFAULT 0,
+  `walk` int(11) NOT NULL DEFAULT 128,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -66,7 +66,7 @@ CREATE TABLE `items` (
   `id` bigint(11) NOT NULL,
   `owner` bigint(11) DEFAULT NULL,
   `item` int(11) DEFAULT NULL,
-  `value` text,
+  `value` text DEFAULT NULL,
   `count` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -82,14 +82,15 @@ DROP TABLE IF EXISTS `vehicles_library`;
 CREATE TABLE `vehicles_library` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gta` int(11) DEFAULT NULL,
-  `brand` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `model` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `year` text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `brand` text DEFAULT NULL,
+  `model` text DEFAULT NULL,
+  `year` text DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `tax` int(11) DEFAULT NULL,
-  `handling` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `enabled` int(11) DEFAULT NULL,
+  `handling` varchar(1000) DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of vehicles_library
