aed61fe4153fbb91bc0fff5b0193269ad0eab932
Bekir Aşkar <bekiraskar7@icloud.com>
2023-03-19 23:59:10 +0300

    Update library.lua

--- resources/[in-game]/dxlibrary/library.lua
+++ resources/[in-game]/dxlibrary/library.lua
@@ -1,19 +1,18 @@
 _functions = [[
+    screen = Vector2(guiGetScreenSize())
+    tick = getTickCount()
 
-	screen = Vector2(guiGetScreenSize())
-	tick = getTickCount()
-
-	function isInBox(x, y, w, h)
-        if isCursorShowing() then
-            local cx, cy = getCursorPosition()
-            cx, cy = cx*screen.x, cy*screen.y
-            if (cx >= x and cx <= x+w and cy >= y and cy <= y+h) then
-                exports.cursor:setPointer(true)
-                return true
-            end
-        end
-        return false
-	end
+    function isInBox(x, y, w, h)
+       if isCursorShowing() then
+          local cx, cy = getCursorPosition()
+ 	  cx, cy = cx*screen.x, cy*screen.y
+       	  if (cx >= x and cx <= x+w and cy >= y and cy <= y+h) then
+              exports.cursor:setPointer(true)
+              return true
+  	  end
+       end
+       return false
+    end
 
     function dxDrawroundedRectangle(x, y, width, height, radius, color)
         local diameter = radius * 2
@@ -27,22 +26,22 @@ _functions = [[
         dxDrawRectangle(x + radius, y + radius, width - diameter, height - diameter, tocolor(0, 0, 0, 0))
     end
 	
-	function dxDrawouterBorder(x, y, w, h, borderSize, borderColor, postGUI)
-		borderSize = borderSize or 2
-		borderColor = borderColor or tocolor(0, 0, 0, 255)
-		dxDrawRectangle(x - borderSize, y - borderSize, w + (borderSize * 2), borderSize, borderColor, postGUI)
-		dxDrawRectangle(x, y + h, w, borderSize, borderColor, postGUI)
-		dxDrawRectangle(x - borderSize, y, borderSize, h + borderSize, borderColor, postGUI)
-		dxDrawRectangle(x + w, y, borderSize, h + borderSize, borderColor, postGUI)
-	end
+    function dxDrawouterBorder(x, y, w, h, borderSize, borderColor, postGUI)
+       borderSize = borderSize or 2
+       borderColor = borderColor or tocolor(0, 0, 0, 255)
+       dxDrawRectangle(x - borderSize, y - borderSize, w + (borderSize * 2), borderSize, borderColor, postGUI)
+       dxDrawRectangle(x, y + h, w, borderSize, borderColor, postGUI)
+       dxDrawRectangle(x - borderSize, y, borderSize, h + borderSize, borderColor, postGUI)
+       dxDrawRectangle(x + w, y, borderSize, h + borderSize, borderColor, postGUI)
+    end
 	
-	function isClicked()
-		if getKeyState("mouse1") and tick < getTickCount() then 
-			tick = getTickCount()+500 
-			return true
-		end
-		return false
-	end
+    function isClicked()
+       if getKeyState("mouse1") and tick < getTickCount() then 
+	  tick = getTickCount()+500 
+	  return true
+       end
+       return false
+    end
 
 ]]
 
