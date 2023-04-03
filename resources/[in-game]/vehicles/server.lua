8c9225f09f16ec87d036d821b915b929a4da1735
Bekir Aşkar <bekiraskar7@icloud.com>
2023-04-03 11:53:20 +0300

    araç yüklemesi için Async kullanıldı

--- resources/[in-game]/vehicles/server.lua
+++ resources/[in-game]/vehicles/server.lua
@@ -1,3 +1,4 @@
+Async:setPriority("low")
 local setElementData = setElementData
 local exports = exports
 local tonumber = tonumber
@@ -6,53 +7,48 @@ local conn = exports.mysql:getConn()
 local cache = exports.cache
 local _print = outputDebugString
 
-dbQuery(
-    function(qh)
-        local res, rows, _ = dbPoll(qh, 0)
-        for index, row in ipairs(res) do
-            local dbid = tonumber(row.id)
-            local libid = tonumber(row.library_id)
-            local x, y, z, int, dim = unpack(split(cache:getVehicleData(dbid, "pos"), ","))
-            local r, g, b = unpack(split(cache:getVehicleData(dbid, "color"), ","))
-            local plate = cache:getVehicleData(dbid, "plate")
-            local lock = cache:getVehicleData(dbid, "lock")
-            local fuel = cache:getVehicleData(dbid, "fuel")
-            local tax = cache:getVehicleData(dbid, "tax")
-            dbQuery(
-                function(qh)
-                    local res, rows, _ = dbPoll(qh, 0)
-                    for index, row in ipairs(res) do
-                        if libid == tonumber(row.id) then
-                            local gta = tonumber(row.gta)
-                            local carshop_price = tonumber(row.price)
-                            local carshop_tax = tonumber(row.tax)
-                            local veh = createVehicle(gta, x, y, z)
-                            if veh then
-                                setVehicleColor(veh, r, g, b)
-                                setElementInterior(veh, int)
-                                setElementDimension(veh, dim)
-                                setVehiclePlateText(veh, plate)
-                                setElementData(veh, "dbid", dbid)
-                                setElementData(veh, "fuel", tonumber(fuel))
-                                setElementData(veh, "tax", tonumber(tax))
-                                setElementData(veh, "carshop_price", tonumber(carshop_price))
-                                setElementData(veh, "carshop_tax", tonumber(carshop_tax))
-                                if tonumber(lock) == 1 then
-                                    setVehicleLocked(veh, true)
-                                else
-                                    setVehicleLocked(veh, false)
-                                end
-                            else
-                                _print("! VEHICLES FAILED CREATE VEHICLE "..dbid)
-                            end
-                            break
+dbQuery(function(qh)
+    local res = dbPoll(qh, 0)
+    Async:foreach(res, function(row)
+        local dbid = tonumber(row.id)
+        local libid = tonumber(row.library_id)
+        local x, y, z, int, dim = unpack(split(cache:getVehicleData(dbid, "pos"), ","))
+        local r, g, b = unpack(split(cache:getVehicleData(dbid, "color"), ","))
+        local plate = cache:getVehicleData(dbid, "plate")
+        local lock = cache:getVehicleData(dbid, "lock")
+        local fuel = cache:getVehicleData(dbid, "fuel")
+        local tax = cache:getVehicleData(dbid, "tax")
+        dbQuery(function(qh)
+            local res = dbPoll(qh, 0) 
+            Async:foreach(res, function(row)
+                if libid == tonumber(row.id) then
+                    local gta = tonumber(row.gta)
+                    local carshop_price = tonumber(row.price)
+                    local carshop_tax = tonumber(row.tax)
+                    local veh = createVehicle(gta, x, y, z)
+                    if veh then
+                        setVehicleColor(veh, r, g, b)
+                        setElementInterior(veh, int)
+                        setElementDimension(veh, dim)
+                        setVehiclePlateText(veh, plate)
+                        setElementData(veh, "dbid", dbid)
+                        setElementData(veh, "fuel", tonumber(fuel))
+                        setElementData(veh, "tax", tonumber(tax))
+                        setElementData(veh, "carshop_price", tonumber(carshop_price))
+                        setElementData(veh, "carshop_tax", tonumber(carshop_tax))
+                        if tonumber(lock) == 1 then
+                            setVehicleLocked(veh, true)
+                        else
+                            setVehicleLocked(veh, false)
                         end
+                    else
+                        _print("! VEHICLES FAILED CREATE VEHICLE "..dbid)
                     end
-                end,
-            conn, "SELECT * FROM vehicles_library")
-        end
-    end,
-conn, "SELECT id,library_id FROM vehicles")
+                end
+            end)
+        end, conn, "SELECT * FROM vehicles_library")
+    end)
+end, conn, "SELECT id,library_id FROM vehicles")
 
 function makeVehicle()
     -- soon
