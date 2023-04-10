86994801686502035e72a36b87208c6da6afac86
Bekir Aşkar <bekiraskar7@icloud.com>
2023-04-10 10:42:06 +0300

    bazı performans eklemeleri.

--- resources/[in-game]/vehicles/client.lua
+++ resources/[in-game]/vehicles/client.lua
@@ -1,7 +1,7 @@
 addEvent("vehicle.effect.3d", true)
 addEventHandler("vehicle.effect.3d", root, function(src, x, y, z)
     local effect = playSound3D(src, x, y, z, false)
-    setSoundMaxDistance(effect, 5)
+    setSoundMaxDistance(effect, 12)
 end)
 
 bindKey("j", "down", function()
@@ -16,6 +16,20 @@ bindKey("j", "down", function()
     end
 
     triggerServerEvent("vehicle.toggle.engine", localPlayer, vehicle)
+end)
+
+bindKey("l", "down", function()
+    if not getElementData(localPlayer, "online") then
+        return
+    end
+    
+    local vehicle = getPedOccupiedVehicle(localPlayer)
+    
+    if not vehicle then
+        return
+    end
+
+    triggerServerEvent("vehicle.toggle.lights", localPlayer, vehicle)
 end)
 
 bindKey("k", "down", function()
--- resources/[in-game]/vehicles/server.lua
+++ resources/[in-game]/vehicles/server.lua
@@ -1,31 +1,49 @@
 Async:setPriority("low")
+local createVehicle = createVehicle
+local setVehicleColor = setVehicleColor
+local setElementInterior = setElementInterior
+local setElementDimension = setElementDimension
+local setVehiclePlateText = setVehiclePlateText
 local setElementData = setElementData
-local exports = exports
+local setVehicleLocked = setVehicleLocked
+local setVehicleEngineState = setVehicleEngineState
+local triggerClientEvent = triggerClientEvent
+local addEvent = addEvent
+local addEventHandler = addEventHandler
 local tonumber = tonumber
-local ipairs = ipairs
+local split = split
+local exports = exports
+local setElementData = setElementData
 local conn = exports.mysql:getConn()
 local cache = exports.cache
 local _print = outputDebugString
+local ipairs = ipairs
 
-dbQuery(function(qh)
-    local res = dbPoll(qh, 0)
-    Async:foreach(res, function(row)
-        local dbid = tonumber(row.id)
-        local libid = tonumber(row.library_id)
-        local x, y, z, int, dim = unpack(split(cache:getVehicleData(dbid, "pos"), ","))
-        local r, g, b = unpack(split(cache:getVehicleData(dbid, "color"), ","))
-        local plate = cache:getVehicleData(dbid, "plate")
-        local lock = cache:getVehicleData(dbid, "lock")
-        local engine = cache:getVehicleData(dbid, "engine")
-        local fuel = cache:getVehicleData(dbid, "fuel")
-        local tax = cache:getVehicleData(dbid, "tax")
+function loadAlllVehicle()
+    local vehLibrary = {}
+    local query = dbQuery(conn, "SELECT id,gta,price,tax FROM vehicles_library")
+    local result = dbPoll(query, -1)
+    if result then
+        for index, value in ipairs(result) do
+            vehLibrary[value.id] = value
+        end
         dbQuery(function(qh)
-            local res = dbPoll(qh, 0) 
+            local res = dbPoll(qh, 0)
             Async:foreach(res, function(row)
-                if libid == tonumber(row.id) then
-                    local gta = tonumber(row.gta)
-                    local carshop_price = tonumber(row.price)
-                    local carshop_tax = tonumber(row.tax)
+                local dbid = tonumber(row.id)
+                local libid = tonumber(row.library_id)
+                local x, y, z, int, dim = unpack(split(cache:getVehicleData(dbid, "pos"), ","))
+                local r, g, b = unpack(split(cache:getVehicleData(dbid, "color"), ","))
+                local plate = cache:getVehicleData(dbid, "plate")
+                local lock = cache:getVehicleData(dbid, "lock")
+                local engine = cache:getVehicleData(dbid, "engine")
+                local fuel = cache:getVehicleData(dbid, "fuel")
+                local tax = cache:getVehicleData(dbid, "tax")
+                local value = vehLibrary[libid]
+                if value then
+                    local gta = tonumber(value.gta)
+                    local carshop_price = tonumber(value.price)
+                    local carshop_tax = tonumber(value.tax)
                     local veh = createVehicle(gta, x, y, z)
                     if veh then
                         setVehicleColor(veh, r, g, b)
@@ -37,24 +55,23 @@ dbQuery(function(qh)
                         setElementData(veh, "tax", tonumber(tax))
                         setElementData(veh, "carshop_price", tonumber(carshop_price))
                         setElementData(veh, "carshop_tax", tonumber(carshop_tax))
-                        if tonumber(lock) == 1 then
-                            setVehicleLocked(veh, true)
-                        else
-                            setVehicleLocked(veh, false)
-                        end
-                        if tonumber(engine) == 1 then
-                            setVehicleEngineState(veh, true)
-                        else
-                            setVehicleEngineState(veh, false)
-                        end
+                        setVehicleLocked(veh, tonumber(lock) == 1)
+                        setVehicleEngineState(veh, tonumber(engine) == 1)
                     else
                         _print("! VEHICLES FAILED CREATE VEHICLE "..dbid)
                     end
+                else
+                    _print("! VEHICLES INVALID LIBRARY ID "..dbid)
                 end
             end)
-        end, conn, "SELECT * FROM vehicles_library")
-    end)
-end, conn, "SELECT id,library_id FROM vehicles")
+        end, conn, "SELECT id,library_id FROM vehicles")
+    end
+end
+addEventHandler("onResourceStart", root, loadAlllVehicle)
+
+function makeVehicle(player)
+    -- soon
+end
 
 function startEnterVehicle(player)
     local vehicle = source
@@ -94,7 +111,7 @@ function toggleVehicleLock()
                     exports.cache:setVehicleData(dbid, "lock", 1)
                     setVehicleLocked(vehicle, true)
                 end
-                triggerClientEvent("vehicle.effect.3d", root, "assets/lock.mp3", vx, vy, vz)
+                triggerClientEvent("vehicle.effect.3d", root, "assets/lock.wav", vx, vy, vz)
                 setPedAnimation(source, "ped", "walk_doorpartial", -1, false, false, false, false)
                 local oldState = getVehicleOverrideLights(vehicle)
                 local from = 2
@@ -112,15 +129,34 @@ end
 addEvent("vehicle.toggle.lock", true)
 addEventHandler("vehicle.toggle.lock", root, toggleVehicleLock)
 
+function toggleVehicleLights(vehicle)
+    local dbid = getElementData(vehicle, "dbid")
+    local engine = cache:getVehicleData(dbid, "engine") or 0
+    if engine == 1 then
+        if getVehicleOverrideLights(vehicle) ~= 2 then
+            setVehicleOverrideLights(vehicle, 2)
+        else
+            setVehicleOverrideLights(vehicle, 1)
+        end
+    end
+end
+addEvent("vehicle.toggle.lights", true)
+addEventHandler("vehicle.toggle.lights", root, toggleVehicleLights)
+
 function toggleVehicleEngine(vehicle)
     local x, y, z = getElementPosition(vehicle)
     local dbid = getElementData(vehicle, "dbid")
     local engine = cache:getVehicleData(dbid, "engine") or 0
     local fuel = getElementData(vehicle, "fuel")
     local job = cache:getVehicleData(dbid, "job")
+    local interest = cache:getVehicleData(dbid, "interest") or 0
 
     if job == 0 then
         if not exports.items:hasItem(source, 2, dbid) then
+            return
+        end
+        if interest == 1 then
+            outputChatBox("Aracınız faize bağlanmış", source, 255, 0, 0)
             return
         end
         if fuel <= 0 then
@@ -130,7 +166,7 @@ function toggleVehicleEngine(vehicle)
     end
 
     if tonumber(engine) == 0 then
-        triggerClientEvent("vehicle.effect.3d", root, "assets/engine.mp3", x, y, z)
+        triggerClientEvent("vehicle.effect.3d", root, "assets/engine.wav", x, y, z)
         setTimer(function()
             local chance = math.random(1, 3)
             if chance == 1 then
@@ -139,15 +175,12 @@ function toggleVehicleEngine(vehicle)
                 setVehicleEngineState(vehicle, true)
                 cache:setVehicleData(dbid, "engine", 1)
             end
-        end, 2500, 1, source, vehicle)
+        end, 1500, 1, source, vehicle)
     else
+        setVehicleOverrideLights(vehicle, 1)
         setVehicleEngineState(vehicle, false)
         cache:setVehicleData(dbid, "engine", 0)
     end
 end
 addEvent("vehicle.toggle.engine", true)
 addEventHandler("vehicle.toggle.engine", root, toggleVehicleEngine)
-
-function makeVehicle(player)
-    -- soon
-end
