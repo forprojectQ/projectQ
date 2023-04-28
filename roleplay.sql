7e707f473b865c8e94a4825bba73410eca4537ca
mahlukat5 <temmuz.sanaga@hotmail.com>
2023-04-28 16:34:43 +0300

    vehicles_custom tablosu eklendi ve vehicles içine upgrades fieldi eklendi.

--- roleplay.sql
+++ roleplay.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : database
-Source Server Version : 80017
+Source Server         : localsw
+Source Server Version : 50505
 Source Host           : localhost:3306
 Source Database       : roleplay
 
 Target Server Type    : MYSQL
-Target Server Version : 80017
+Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2023-04-10 10:26:38
+Date: 2023-04-28 16:32:57
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,18 +20,18 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
   `id` int(11) NOT NULL,
-  `name` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
-  `password` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
-  `admin` int(11) NOT NULL DEFAULT '0',
-  `serial` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
-  `limit` int(11) NOT NULL DEFAULT '3',
+  `name` text CHARACTER SET latin1 DEFAULT NULL,
+  `password` text CHARACTER SET latin1 DEFAULT NULL,
+  `admin` int(11) NOT NULL DEFAULT 0,
+  `serial` text CHARACTER SET latin1 DEFAULT NULL,
+  `limit` int(11) NOT NULL DEFAULT 3,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of accounts
 -- ----------------------------
-INSERT INTO `accounts` VALUES ('1', 'test', 'test', '0', 'B5D767EFFB542805FE49564D79C68A54', '3');
+INSERT INTO `accounts` VALUES ('1', 'test', 'test', '10', 'B5D767EFFB542805FE49564D79C68A54', '3');
 
 -- ----------------------------
 -- Table structure for `characters`
@@ -39,27 +39,27 @@ INSERT INTO `accounts` VALUES ('1', 'test', 'test', '0', 'B5D767EFFB542805FE4956
 DROP TABLE IF EXISTS `characters`;
 CREATE TABLE `characters` (
   `id` int(11) NOT NULL,
-  `account` int(11) NOT NULL DEFAULT '0',
-  `name` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
+  `account` int(11) NOT NULL DEFAULT 0,
+  `name` text CHARACTER SET latin1 DEFAULT NULL,
   `pos` varchar(255) NOT NULL DEFAULT '0,0,5,0,0',
-  `age` int(11) NOT NULL DEFAULT '18',
-  `height` int(11) NOT NULL DEFAULT '175',
-  `weight` int(11) NOT NULL DEFAULT '65',
-  `gender` int(1) NOT NULL DEFAULT '1',
-  `model` int(11) NOT NULL DEFAULT '170',
-  `money` bigint(11) NOT NULL DEFAULT '1000',
-  `hunger` int(11) NOT NULL DEFAULT '100',
-  `thirst` int(11) NOT NULL DEFAULT '100',
-  `dead` int(1) NOT NULL DEFAULT '0',
-  `walk` int(11) NOT NULL DEFAULT '128',
-  `job` int(11) NOT NULL DEFAULT '0',
+  `age` int(11) NOT NULL DEFAULT 18,
+  `height` int(11) NOT NULL DEFAULT 175,
+  `weight` int(11) NOT NULL DEFAULT 65,
+  `gender` int(1) NOT NULL DEFAULT 1,
+  `model` int(11) NOT NULL DEFAULT 170,
+  `money` bigint(11) NOT NULL DEFAULT 1000,
+  `hunger` int(11) NOT NULL DEFAULT 100,
+  `thirst` int(11) NOT NULL DEFAULT 100,
+  `dead` int(1) NOT NULL DEFAULT 0,
+  `walk` int(11) NOT NULL DEFAULT 128,
+  `job` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of characters
 -- ----------------------------
-INSERT INTO `characters` VALUES ('1', '1', 'test', '0,0,5,0,0', '21', '175', '70', '2', '93', '1000', '78', '71', '0', '131', '0');
+INSERT INTO `characters` VALUES ('1', '1', 'test', '0,0,5,0,0', '21', '175', '70', '2', '93', '1000', '63', '59', '0', '131', '0');
 
 -- ----------------------------
 -- Table structure for `items`
@@ -70,7 +70,7 @@ CREATE TABLE `items` (
   `type` int(11) DEFAULT NULL,
   `owner` bigint(11) DEFAULT NULL,
   `item` int(11) DEFAULT NULL,
-  `value` text,
+  `value` text DEFAULT NULL,
   `count` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -78,6 +78,12 @@ CREATE TABLE `items` (
 -- ----------------------------
 -- Records of items
 -- ----------------------------
+INSERT INTO `items` VALUES ('31', '1', '1', '2', '13', '1');
+INSERT INTO `items` VALUES ('32', '1', '1', '2', '14', '1');
+INSERT INTO `items` VALUES ('33', '1', '1', '2', '15', '1');
+INSERT INTO `items` VALUES ('34', '1', '1', '2', '16', '1');
+INSERT INTO `items` VALUES ('35', '1', '1', '2', '17', '1');
+INSERT INTO `items` VALUES ('36', '1', '1', '2', '18', '1');
 
 -- ----------------------------
 -- Table structure for `vehicles`
@@ -86,22 +92,47 @@ DROP TABLE IF EXISTS `vehicles`;
 CREATE TABLE `vehicles` (
   `id` int(11) NOT NULL,
   `library_id` int(11) DEFAULT NULL,
-  `fuel` int(11) NOT NULL DEFAULT '100',
-  `tax` int(11) NOT NULL DEFAULT '0',
+  `fuel` int(11) NOT NULL DEFAULT 100,
+  `tax` int(11) NOT NULL DEFAULT 0,
   `job` int(11) DEFAULT NULL,
-  `pos` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0,0,5,0,0,0,0,90',
-  `plate` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `color` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0,0,0',
-  `lock` int(11) NOT NULL DEFAULT '1',
-  `interest` int(11) NOT NULL DEFAULT '0',
+  `pos` varchar(255) NOT NULL DEFAULT '0,0,5,0,0,0,0,90',
+  `plate` text DEFAULT NULL,
+  `color` varchar(255) NOT NULL DEFAULT '0,0,0',
+  `upgrades` text DEFAULT '',
+  `lock` int(11) NOT NULL DEFAULT 1,
+  `interest` int(11) NOT NULL DEFAULT 0,
   `owner` int(11) DEFAULT NULL,
-  `engine` int(11) NOT NULL DEFAULT '0',
-  `enabled` int(1) NOT NULL DEFAULT '1',
+  `engine` int(11) NOT NULL DEFAULT 0,
+  `enabled` int(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of vehicles
+-- ----------------------------
+INSERT INTO `vehicles` VALUES ('13', '1', '100', '0', '0', '8.5634580278042,8.7875766467764,3.1096496582031,0,0,0,0,100.23803710938', 'AOF-865', '0,0,0', null, '1', '0', '1', '0', '1');
+INSERT INTO `vehicles` VALUES ('14', '1', '100', '0', '0', '8.0699133685011,-2.7380825766116,3.1171875,0,0,0,0,206.74113464355', 'JXV-728', '0,0,0', null, '1', '0', '1', '0', '1');
+INSERT INTO `vehicles` VALUES ('15', '1', '100', '0', '0', '27.784453625541,-13.538959302451,3.1171875,0,0,0,0,255.99908447266', 'SNH-114', '0,0,0', null, '1', '0', '1', '0', '1');
+INSERT INTO `vehicles` VALUES ('16', '1', '100', '0', '0', '34.089652932674,5.7571424293423,3.1171875,0,0,0,0,18.201873779297', 'DEZ-521', '0,0,0', null, '1', '0', '1', '0', '1');
+INSERT INTO `vehicles` VALUES ('17', '1', '100', '0', '0', '21.632355946983,-6.9611503467267,3.1171875,0,0,0,0,332.96499633789', 'AJN-666', '0,0,0', null, '1', '0', '1', '0', '1');
+INSERT INTO `vehicles` VALUES ('18', '1', '100', '0', '0', '49.476451372852,-31.174437523343,1.1405982971191,0,0,0,0,58.582733154297', 'ERL-416', '0,0,0', null, '1', '0', '1', '0', '1');
+
+-- ----------------------------
+-- Table structure for `vehicles_custom`
+-- ----------------------------
+DROP TABLE IF EXISTS `vehicles_custom`;
+CREATE TABLE `vehicles_custom` (
+  `id` int(11) NOT NULL,
+  `brand` text DEFAULT NULL,
+  `model` text DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `tax` int(11) DEFAULT NULL,
+  `handling` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of vehicles_custom
 -- ----------------------------
 
 -- ----------------------------
@@ -111,9 +142,9 @@ DROP TABLE IF EXISTS `vehicles_library`;
 CREATE TABLE `vehicles_library` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gta` int(11) DEFAULT NULL,
-  `brand` text,
-  `model` text,
-  `year` text,
+  `brand` text DEFAULT NULL,
+  `model` text DEFAULT NULL,
+  `year` text DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `tax` int(11) DEFAULT NULL,
   `enabled` int(11) DEFAULT NULL,
