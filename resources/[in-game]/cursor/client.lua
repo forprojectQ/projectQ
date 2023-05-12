ea5eba6c0e4f197197a33190a1fcf27514eec934
Bekir Aşkar <bekiraskar7@icloud.com>
2023-05-12 03:16:38 +0300

    imleç için ufak düzeltme.

--- resources/[in-game]/cursor/client.lua
+++ resources/[in-game]/cursor/client.lua
@@ -1,5 +1,14 @@
 local cursor = "arrow"
 local screen = Vector2(guiGetScreenSize())
+local active_gui = nil
+local gui_cursor_types={
+    ["gui-edit"]="text",
+    ["gui-memo"]="text",
+    ["gui-button"]="hand",
+    ["gui-checkbox"]="hand",
+}
+
+setCursorAlpha(0)
 
 function setPointer(pointer)
     if pointer then
@@ -11,25 +20,18 @@ bindKey("m", "down", function()
     local state = not isCursorShowing()
     showCursor(state)
     if state then
-        cursor = "arrow"
+        cursor, active_gui = "arrow", nil
         setCursorPosition(screen.x / 2, screen.y / 2)
     end
 end)
 
-gui_cursor_types={
-    ["gui-edit"]="text",
-    ["gui-memo"]="text",
-    ["gui-button"]="hand",
-    ["gui-checkbox"]="hand",
-}
-
-local active_gui = nil
 addEventHandler("onClientMouseEnter",root,function()
     local type = getElementType(source)
     if gui_cursor_types[type] then
         active_gui,cursor=source,gui_cursor_types[type]
     end    
 end)
+
 addEventHandler("onClientMouseLeave",root,function()
     local type = getElementType(source)
     if gui_cursor_types[type] then
@@ -47,5 +49,3 @@ addEventHandler("onClientRender", root, function()
         end
     end
 end, true, "low-9999")
-
-setCursorAlpha(0)
